eed18cf9d648c76c18d382f0641f137806f2246b
iamkarthikkasula99 <iamkarthikkasula99@gmail.com>
2024-11-24 20:39:42 -0600

    added all dogmanagement classes and docker file

---
+++ src/main/java/com/bandiClasses/DMS/DogManagementSystemApplication.java
@@ -0,0 +1,20 @@
+package com.bandiClasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+ * @author Kasula karthik (s572203)
+ * 
+ */
+
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandiClasses/DMS/DogManagementSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bandiClasses.DMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+ * @author Kasula karthik (s572203)
+ * 
+ */
+@SpringBootTest
+class DogManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
